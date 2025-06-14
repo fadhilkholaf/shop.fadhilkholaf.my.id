245b2b0b1ee780ef43fe134247b59346295af013
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2025-06-14 22:29:02 +0700

    chore: app settings

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        remotePatterns: [
-            { hostname: "avatars.githubusercontent.com" },
-            { hostname: "res.cloudinary.com" },
-        ],
         unoptimized: true,
     },
 };
