09388a126599022ba29c8587d142e09d7778a738
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2025-06-23 18:59:03 +0700

    feat: update privacy policy

--- src/app/(legal)/privacy-policy/page.tsx
+++ src/app/(legal)/privacy-policy/page.tsx
@@ -1,26 +1,41 @@
+import MarkdownRenderer from "@/components/MarkdownRenderer";
+
+const privacyPolicy = `
+*Last update: June 22, 2025.*
+
+# Privacy Policy
+
+How we collect users data.
+
+## Collected Data
+
+We collected users data from GitHub such as: 
+
+- User id.
+- User name.
+- User profile photo.
+
+## Purpose of the Data
+
+Data collected is used for working with GitHub API and managing users session.
+
+## Data Sharing
+
+We don't share collected user data outside our application.
+
+## How Long the Data is Collected
+
+The data is don't have expiration time unless the user requested a deletion.
+
+## Data Deletion
+
+Users can request data deletion personally through this email [fadhilkholafbusiness@gmail.com](mailto:fadhilkholafbusiness@gmail.com)
+`;
+
 export default function PrivacyPolicyPage() {
     return (
         <main className="layout my-16">
-            <h1>Privacy Policy</h1>
-            <ul className="list-disc pl-4">
-                <li>
-                    We only collect minimal data from your GitHub accout, such
-                    as:
-                    <ul className="list-disc pl-4">
-                        <li>User id</li>
-                        <li>Username</li>
-                        <li>Profile image</li>
-                    </ul>
-                </li>
-                <li>
-                    And data from PayPal payment API like:
-                    <ul className="list-disc pl-4">
-                        <li>Order id</li>
-                        <li>Capture id</li>
-                    </ul>
-                </li>
-            </ul>
-            <p>{`That's it, nothing else`}</p>
+            <MarkdownRenderer>{privacyPolicy}</MarkdownRenderer>
         </main>
     );
 }
